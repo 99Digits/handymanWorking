648c5b2462831a215ef87de330e0f024b59dfd16
99digits <yadu@99digits.com>
2023-07-26 09:51:00 +0530

    user status code changed

--- api/usercreation/usercreation.controller.js
+++ api/usercreation/usercreation.controller.js
@@ -70,7 +70,7 @@ module.exports ={
             }
 
             else if(results.length==0){
-                return res.status(200).json({
+                return res.status(203).json({
                     success:1,
                     message:"username and password id does not match"
                 })
